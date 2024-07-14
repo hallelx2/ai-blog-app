a1926b69bcd344a8bf93a4ea55aa6847f9b70b97
Halleluyah <98324073+hallelx2@users.noreply.github.com>
2024-07-14 15:32:33 +0100

    Updated settings.py

--- ai_blog_app/ai_blog_app/settings.py
+++ ai_blog_app/ai_blog_app/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production! 
 DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ai-blog-app-aye3.onrender.com', 'ai-blog-app.pipeops.app']
